0ffe1e81092594373f48346575c8c1de2bf90573
Seth Danner <sethdanner@Seths-iMac.local>
2019-01-15 14:09:29 -0700

    First commit.

---
+++ Magic8Ball/EightBallViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EightBallViewController.swift
+//  Magic8Ball
+//
+//  Created by Seth Danner on 9/21/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import UIKit
+
+class EightBallViewController: UIViewController {
+    
+    @IBOutlet weak var eightBallImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func eightBallButtonTapped(_ sender: UIButton) {
+
+//        UIView.shake()
+    }
+}
