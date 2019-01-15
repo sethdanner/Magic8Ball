cd1304f10b8728ffc872835bccd87772153142cb
Seth Danner <sethdanner@Seths-iMac.local>
2019-01-15 16:13:27 -0700

    Added images, ballButton, ballImageView, and animation function.

--- Magic8Ball/EightBallViewController.swift
+++ Magic8Ball/EightBallViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class EightBallViewController: UIViewController {
     
-    @IBOutlet weak var eightBallImage: UIImageView!
+    // MARK: Properties
+    @IBOutlet weak var ballImageView: UIImageView!
+    
+    var ballImages: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        ballImages = createImageArray(total: 8, imagePrefix: "MEB")
+        
     }
     
-    @IBAction func eightBallButtonTapped(_ sender: UIButton) {
-
-//        UIView.shake()
+    func createImageArray(total: Int, imagePrefix: String) -> [UIImage] {
+        
+        var imageArray: [UIImage] = []
+        
+        for imageCount in 0..<total {
+            let imageName = "\(imagePrefix)-\(imageCount).png"
+            print(imageName)
+            let image = UIImage(named: imageName)!
+            
+            imageArray.append(image)
+        }
+        
+        return imageArray
+    }
+    
+    func animate(imageView: UIImageView, images: [UIImage]) {
+        
+        imageView.animationImages = images
+        imageView.animationDuration = 1.0
+        imageView.animationRepeatCount = 3
+        imageView.startAnimating()
+    }
+    
+    @IBAction func ballButtonTapped(_ sender: UIButton) {
+        
+        animate(imageView: ballImageView, images: ballImages)
     }
 }
